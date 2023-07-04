2d4c37b34826b8fcd041b55fd0f8fc7941d9dd54
PiyushChandra17 <chandra.piyush17@gmail.com>
2023-07-04 13:32:50 +0530

    fixed

--- config.js
+++
@@ -1,4 +0,0 @@
-exports.DATABASE = "mongodb+srv://piyush_js:piyush_js@ipl-news-app.xgkinju.mongodb.net/?retryWrites=true&w=majority";
-exports.JWT_SECRET = "123abc";
-exports.SENDGRID_KEY = "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";
-exports.EMAIL_FROM = "piyushblr5@gmail.com";
