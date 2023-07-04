eeadf1a817543b3a6c67ef25958a3588708e2ce7
PiyushChandra17 <chandra.piyush17@gmail.com>
2023-07-04 16:37:18 +0530

    code

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,10 @@ const dotenv = require('dotenv');
 // sendgrid
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey("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");
+sgMail.setApiKey(process.env.SENDGRID_KEY);
+
+console.log(process.env.SENDGRID_KEY)
+
 
 // middleware
 exports.requireSignin = expressJwt({
